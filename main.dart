49ae16eddf7779259f7ed6fd2acc248facab3118
Danielle Almeida <daniellealmeidagms@gmail.com>
2023-07-03 15:27:32 -0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BaseApp',
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("BaseApp"),
+        backgroundColor: Colors.indigo,
+      ),
+      body: const Center(),
+    );
+  }
+}
